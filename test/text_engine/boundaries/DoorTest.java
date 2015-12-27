b7c137db5ff798006956221cfe6cae767aaa6074
Logan Moore <lmooore@gmail.com>
2015-12-26 17:12:36 -0800

    Added test class skeleton

---
+++ test/text_engine/boundaries/DoorTest.java
@@ -0,0 +1,14 @@
+package text_engine.boundaries;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link Door}s.
+ */
+public class DoorTest {
+
+  @Test
+  public void testName() throws Exception {
+
+  }
+}
