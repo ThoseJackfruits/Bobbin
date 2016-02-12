a585a2d738a3c04c222282684b596e9526a7663c
Logan Moore <lmooore@gmail.com>
2016-02-12 11:07:39 -0500

    Various key and room related changes
    - Allowed doors to have rooms added or changed after their creation
    (could allow for some cool puzzle / map-changing mechanics)
    - Added methods to both lock and unlock the door, provided a proper key.

--- src/text_engine/boundaries/Door.java
+++ src/text_engine/boundaries/Door.java
@@ -1,5 +1,7 @@
 package text_engine.boundaries;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.Objects;
 import java.util.Random;
 
@@ -12,95 +14,165 @@ import text_engine.items.Key;
  */
 public class Door {
 
-    private final Room room1;
-    private final Room room2;
-    private boolean locked;
-    private final long lock;
+  private Room room1;
+  private Room room2;
+  private boolean locked;
+  private final long lock;
 
-    /**
-     * Constructs a text_engine.boundaries.Door object
-     *
-     * @param room1 The room on one side of the {@link Door}
-     * @param room2 The room on the other side of the {@link Door}
-     * @param locked Whether this {@link Door} is locked
-     */
-    Door(Room room1, Room room2, boolean locked) {
-        Objects.requireNonNull(room1);
-        Objects.requireNonNull(room2);
+  /**
+   * Constructs a text_engine.boundaries.Door object
+   *
+   * @param room1  the room on one side of the {@link Door}
+   * @param room2  the room on the other side of the {@link Door}
+   * @param locked whether this {@link Door} is locked
+   */
+  Door(Room room1, Room room2, boolean locked) {
+    this(locked);
+    Objects.requireNonNull(room1);
+    Objects.requireNonNull(room2);
 
-        this.room1 = room1;
-        this.room2 = room2;
-        this.locked = locked;
+    this.room1 = room1;
+    this.room2 = room2;
+  }
 
-        lock = new Random().nextLong();
+  /**
+   * Construct a new {@link Door} without any room associations;
+   *
+   * @param locked whether this {@link Door} is locked
+   */
+  public Door(boolean locked) {
+    this.locked = locked;
+
+    lock = new Random().nextLong();
+  }
+
+
+  /**
+   * Set the first {@link Room} that this door is connected to.
+   *
+   * @param room1 the room on one side of the {@link Door}
+   */
+  public void setRoom1(@NotNull Room room1) {
+    Objects.requireNonNull(room1);
+
+    this.room1 = room1;
+  }
+
+
+  /**
+   * Set the first {@link Room} that this door is connected to.
+   *
+   * @param room2 the room on the other side of the {@link Door}
+   */
+  public void setRoom2(@NotNull Room room2) {
+    Objects.requireNonNull(room2);
+
+    this.room2 = room2;
+  }
+
+  /**
+   * Set both {@link Room}s that this door is connected to.
+   *
+   * @param room1 the room on one side of the {@link Door}
+   * @param room2 the room on the other side of the {@link Door}
+   */
+  public void setRooms(@NotNull Room room1, @NotNull Room room2) {
+    setRoom1(room1);
+    setRoom2(room2);
+  }
+
+  /**
+   * Access the room on the other side of the {@link Door}
+   *
+   * @param from the room you're coming from
+   * @return the room on the other side
+   */
+  public Room getOtherRoom(Room from) {
+    Objects.requireNonNull(from);
+    if (!locked) {
+      if (from.equals(this.room1)) {
+        return this.room2;
+      } else if (from.equals(this.room2)) {
+        return this.room1;
+      } else {
+        throw new IllegalArgumentException("Given room is not connected to this door");
+      }
+    } else {
+      throw new IllegalStateException("text_engine.boundaries.Door is locked.");
+    }
+  }
+
+  /**
+   * Generate a description of the {@link Door} based on the rooms it connects.
+   *
+   * @return The generated description.
+   */
+  @Override
+  public String toString() {
+    StringBuilder result = new StringBuilder("Door between ")
+      .append(room1.getName())
+      .append(" and ")
+      .append(room2.getName())
+      .append(".");
+    if (locked) {
+      result.append(" (locked)");
     }
 
-    /**
-     * Access the room on the other side of the {@link Door}
-     *
-     * @param from The room you're coming from
-     * @return The room on the other side
-     */
-    public Room getOtherRoom(Room from) {
-        Objects.requireNonNull(from);
-        if (!locked) {
-            if (from.equals(this.room1)) {
-                return this.room2;
-            } else if (from.equals(this.room2)) {
-                return this.room1;
-            } else {
-                throw new IllegalArgumentException("Given room is not connected to this door");
-            }
-        } else {
-            throw new IllegalStateException("text_engine.boundaries.Door is locked.");
-        }
+    return result.toString();
+  }
+
+
+  /**
+   * Tries to unlock {@link this} {@link Door} with the given {@link Key}.
+   *
+   * @param key the key used to unlock the {@link Door}
+   * @return {@code true} if successfully unlocked, {@code false} otherwise
+   */
+  public boolean unlock(Key key) {
+    return setLocked(false, key);
+  }
+
+  /**
+   * Tries to lock {@link this} {@link Door} with the given {@link Key}.
+   *
+   * @param key the key used to lock the {@link Door}
+   * @return {@code true} if successfully lock, {@code false} otherwise
+   */
+  public boolean lock(Key key) {
+    return setLocked(true, key);
+  }
+
+  /**
+   * Tries to change the lock of {@link this} {@link Door} with the given {@link Key}.
+   *
+   * @param key        the key used to lock the {@link Door}
+   * @param toBeLocked whether the door is to be locked or unlocked
+   * @return {@code true} if successfully lock, {@code false} otherwise
+   */
+  public boolean setLocked(boolean toBeLocked, Key key) {
+    if (fits(key)) {
+      this.locked = toBeLocked;
+      return true;
     }
-
-    /**
-     * Generate a description of the {@link Door} based on the rooms it connects.
-     *
-     * @return The generated description.
-     */
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder("text_engine.boundaries.Door between ")
-          .append(room1.getName())
-          .append(" and ")
-          .append(room2.getName())
-          .append(".");
-        if (locked) {
-            result.append(" This door is locked.");
-        }
-
-        return result.toString();
-    }
+    return false;
+  }
 
 
-    /**
-     * Tries to unlock the {@link Door} with the given {@link Key}.
-     *
-     * @param key the key used to unlock the {@link Door}
-     * @return {@code true} if successfully unlocked, {@code false} otherwise
-     */
-    public boolean unlock(Key key) {
-        if (fits(key)) {
-            locked = false;
-            return true;
-        }
-        return false;
-    }
+  /**
+   * Makes a key that fits this {@link Door};
+   *
+   * @param name name of the {@link Key}
+   * @return key that fits this {@link Door}
+   */
+  public Key makeKey(String name, String description) {
+    return new Key(name, description, Long.hashCode(lock));
+  }
 
-    /**
-     * Makes a key that fits this {@link Door};
-     *
-     * @param name name of the {@link Key}
-     * @return key that fits this {@link Door}
-     */
-    public Key makeKey(String name, String description) {
-        return new Key(name, description, Long.hashCode(lock));
-    }
+  public boolean fits(Key key) {
+    return key.hashCode() == Long.hashCode(lock);
+  }
 
-    public boolean fits(Key key) {
-        return key.hashCode() == Long.hashCode(lock);
-    }
+  public boolean isLocked() {
+    return locked;
+  }
 }
