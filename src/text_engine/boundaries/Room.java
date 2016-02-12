eb2c7e97bb8c1dcdffe374eb6e3898b52597f30a
Logan Moore <lmooore@gmail.com>
2016-02-12 11:10:03 -0500

    Added more post-construction flexibility
    - `exits` and `contents` can now be edited after the fact.
    - Added some more minimal constructors to allow for late initialisation
    and to allow `Door`s and `Room`s to keep references of each other.

--- src/text_engine/boundaries/Room.java
+++ src/text_engine/boundaries/Room.java
@@ -1,6 +1,10 @@
 package text_engine.boundaries;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 
 import text_engine.items.Item;
@@ -12,53 +16,113 @@ import text_engine.items.Item;
  */
 public class Room {
 
-    private final String name;
-    private final ArrayList<Item> contents;
-    private final Door[] exits;
+  public static final int CONTENT_LIMIT = 10;
+  /*
+  INVARIANTS:
+  - `exits` can hold only one copy of a given door (all Doors in `exits` must be unique)
+   */
 
-    /**
-     * Constructs a text_engine.boundaries.Room object
-     *
-     * @param name The name of the room
-     * @param contents The items in the room
-     * @throws IllegalArgumentException If the text_engine.items.Item[] is too large
-     */
-    Room(String name, ArrayList<Item> contents, Door[] exits) {
-        Objects.requireNonNull(name);
-        Objects.requireNonNull(contents);
-        Objects.requireNonNull(exits);
+  private final String name;
+  private final ArrayList<Item> contents = new ArrayList<>();
+  private final ArrayList<Door> exits = new ArrayList<>();
 
-        if (contents.size() > 10) {
-            throw new IllegalArgumentException("Too many items");
-        }
+  /**
+   * Constructs a {@link Room}, with an initial set of {@param exits}.
+   *
+   * @param name  The name of the room
+   * @param exits The initial exits for the room
+   * @throws IllegalArgumentException If the text_engine.items.Item[] is too large ({@code #size() >
+   *                                  10})
+   */
+  public Room(@NotNull String name, Door... exits) {
+    this(name);
 
-        this.name = name;
-        this.contents = contents;
-        this.exits = exits;
+    Collections.addAll(this.exits, exits);
+  }
+
+  /**
+   * Constructs a {@link Room}, with an initial set of {@param contents}.
+   *
+   * @param name     The name of the room
+   * @param contents The initial contents of the room
+   * @throws IllegalArgumentException If the text_engine.items.Item[] is too large ({@code #size() >
+   *                                  10})
+   */
+  public Room(@NotNull String name, Item... contents) {
+    this(name);
+
+    if (contents.length > CONTENT_LIMIT) {
+      throw new IllegalArgumentException(String.format("Rooms can have a maximum of %d items.",
+                                                       CONTENT_LIMIT));
     }
 
-    /**
-     * Examine the room
-     *
-     * @return A concatenation of all items' descriptions and door locations
-     */
-    public String examine() {
-        String result = "";
-        for (Item i: this.contents) {
-            result += i.toString() + "\n";
-        }
-        for (Door d: this.exits) {
-            result += d.toString() + "\n";
-        }
-        return result;
+    Collections.addAll(this.contents, contents);
+  }
+
+  /**
+   * Constructs a {@link Room}, only a name (no contents or doors).
+   *
+   * @param name the name of the room
+   */
+  public Room(@NotNull String name) {
+    Objects.requireNonNull(name);
+    this.name = name;
+  }
+
+  /**
+   * Adds new {@link Door}s as exits to {@link this} room.
+   *
+   * @param exits {@link Door}s to be added
+   */
+  public void addExits(Door... exits) {
+    Arrays.stream(exits)
+          .filter((door) -> !this.exits.contains(door))
+          .map(this.exits::add);
+  }
+
+  /**
+   * Adds new {@link Item}s as contents to {@link this} room.
+   *
+   * @param contents {@link Item}s to be added
+   * @throws IllegalArgumentException if the number of new, unique contents + current contents are
+   *                                  more than the {@link #CONTENT_LIMIT}.
+   */
+  public void addContents(Item... contents) {
+    Item[] cleanedContents = Arrays.stream(contents)
+                                   .filter((item) -> !this.contents.contains(item))
+                                   .toArray(Item[]::new);
+
+    if (cleanedContents.length + this.contents.size() > 10) {
+      throw new IllegalArgumentException(String.format("Rooms can have a maximum of %d items.",
+                                                       CONTENT_LIMIT));
     }
 
-    /**
-     *
-     * @return The name
-     */
-    public String getName() {
-        return this.name;
+    Collections.addAll(this.contents, cleanedContents);
+  }
+
+  /**
+   * Examine the room, collecting all available objects into a string.
+   *
+   * @return A concatenation of all items' descriptions and door locations
+   */
+  public String examine() {
+    String result = "";
+    for (Item i : this.contents) {
+      result += i.toString() + "\n";
     }
+    for (Door d : this.exits) {
+      result += d.toString() + "\n";
+    }
+    return result;
+  }
+
+  /**
+   * Get the name of {@link this} {@link Room}.
+   *
+   * @return The name of {@link this} {@link Room}
+   */
+  public String getName() {
+    return this.name;
+  }
 
 }
