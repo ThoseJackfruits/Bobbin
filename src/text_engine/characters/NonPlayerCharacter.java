6348413acacc0ae409b80bca951ad615bf99f706
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-02-12 14:20:14 -0500

    Added NPC's
    They just extend the Player class for now. As of yet, they don’t do
    anything Players don’t do.

---
+++ src/text_engine/characters/NonPlayerCharacter.java
@@ -0,0 +1,27 @@
+package text_engine.characters;
+
+import java.util.ArrayList;
+
+import text_engine.boundaries.Room;
+import text_engine.items.Item;
+
+/**
+ * Created by JackDavis on 2/12/16.
+ *
+ * Represents an NPC (Non Player Character)
+ */
+public class NonPlayerCharacter extends Player {
+
+  /**
+   * Constructs a text_engine.characters.NonPlayerCharacter object
+   *
+   * @param inventory The list of items in the text_engine.characters.NonPlayerCharacter's possession
+   * @param location The room the NPC is in
+   */
+  NonPlayerCharacter(ArrayList<Item> inventory, Room location) {
+    super(inventory, location);
+  }
+
+  
+
+}
